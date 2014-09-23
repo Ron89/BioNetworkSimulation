026f95c082e5e5f7cdccc76fbf94cf38e2f808a3
Ron89 <chong.he.1989@gmail.com>
2014-09-23 23:19:45 +0800

    change storage scheme for distribution class

--- source/distribution.h
+++ source/distribution.h
@@ -2,6 +2,7 @@
 #define __DISTRIBUTION_H_INCLUDED__
 #include <algorithm>
 #include <cstdio>
+#include <cstdlib>
 #include <fstream>
 #include <sstream>
 #include<unistd.h>
@@ -11,6 +12,7 @@
 
 #define DISTRIBUTION_SIZECAP 4000000000
 #define DIST_ELEMENT_DESCRIPTION "/element_description"
+#define DIST_BOUNDARY_DESCRIPTION "/boundary_description"
 #define DIST_COUNTING "/counting"
 #define MAX_VARIABLE 10
 
@@ -30,17 +32,21 @@ public:
 	int compressLevel[MAX_VARIABLE]; 	// a compress level for each observer.
 	int compressLevel_optimized[MAX_VARIABLE]; 	// a compress level for each observer.
 	string resultFolder;
+	fstream countFile;
 	long int allocatedSize;
 	long int allocatedSize_optimized;
 	long * observer;
 	bool flag_Exceed;
+
 	bool flag_Storage; 	// storage allocated
+	bool countFile_open;
 
 	bool sampleTaken;
 
 	~distribution()
 	{
 		delStorage();
+		unloadDistribution();
 	}
 
 // storage manipulation
@@ -63,11 +69,15 @@ public:
 		return observer[IDextraction(element_alias)]++;
 	}
 
+// constructors
 // class constructor for simulation based evaluation
 	distribution(int nObserver_alias, int * observerRange_alias, string & resultFolder_alias)
 	{
-		struct stat sb; 	// check existance of folder
+// initiate flags
 		flag_Storage=0;
+		countFile_open=0;
+		flag_Exceed=0;	
+		sampleTaken=0; 	// so that when the first sample is taken, description can be complete
 
 		nObserver=nObserver_alias;
 		for (int i=0; i<nObserver; i++)
@@ -77,27 +87,8 @@ public:
 		
 		initiateScheme(); 	// initiating storage scheme
 		optimizeScheme(); 	// optimize compress level of the distribution for memory
-
-		fstream filePointer;
+	// set result folder to the user specified one.
 		resultFolder=resultFolder_alias;
-
-		sampleTaken=0; 	// so that when the first sample is taken, description can be complete
-
-		// generate folder if not exist
-		if (stat(resultFolder.c_str(), &sb) != 0) 	mkdir(resultFolder.c_str(),0755);
-		flag_Exceed=0;	
-		// write the storage scheme to file
-		filePointer.open((resultFolder+DIST_ELEMENT_DESCRIPTION).c_str(), ios::out | ios::trunc);
-		filePointer<<"# This file contains the description for the distribution.\n";
-		filePointer<<"# Observer Number: \n\t"<<nObserver<<endl;
-		filePointer<<"# Range:\n\t";
-		for (int i=0;i<nObserver;i++)
-			filePointer<<observerRange[i]<<'\t';
-		filePointer<<"\n# Compress level: \n\t";
-		for (int i=0;i<nObserver;i++)
-			filePointer<< compressLevel[i]<<'\t';
-		filePointer<<endl;
-		filePointer.close();
 
 		// allocate memory for the distribution
 		// initialize observer
@@ -106,7 +97,43 @@ public:
 	}
 
 // class constructor for loading from file
-	distribution(int nObserver_alias, int)<++>
+// for memory space reservation, we won't load the distribution right after
+// the constructor is loaded. Users may use 
+// 		* pop_element(ID_alias, count_alias)
+// 		* load_distribution()
+// to load the distribution for different purpose
+	distribution(string & resultFolder_alias)
+	{
+		// initiate flags;
+		flag_Storage=0;	
+		flag_Exceed=0;
+		countFile_open=0;
+		sampleTaken=0; 	// so that when the first sample is taken, description can be complete
+
+		resultFolder=resultFolder_alias;
+		openDistribution();
+	}
+	void openDistribution(bool write=0)
+	{
+		if(countFile_open) 	countFile.close();
+		if(write==0)
+		{
+			loadScheme();
+			countFile.open(resultFolder+DIST_COUNTING, ios::in);
+		}
+		if(write==1)
+		{
+			countFile.open(resultFolder+DIST_COUNTING, ios::out | ios::trunc);	
+		}
+		countFile_open=1;
+		sampleTaken=1; 	// correctly saved distribution always have samples stored within it.
+	}
+	void unloadDistribution()
+	{
+		if(countFile_open) 	countFile.close();
+		countFile_open=0;
+		sampleTaken=0; 	
+	}
 
 // extract ID from element input.
 	long IDextraction(int * element_alias)
@@ -129,6 +156,7 @@ public:
 			element_alias[i]=observerZero[i]+
 			tempID%(observerRange[i]/compressLevel[i])*compressLevel[i]+compressLevel[i]/2;
 			tempID/=(observerRange[i]/compressLevel[i]);
+			if(element_alias[i]<lowBound[i]) 	element_alias[i]+=observerRange[i];
 		}
 	}
 
@@ -138,18 +166,10 @@ public:
 		int tempNum;
 		if (sampleTaken==0) 	// when it's the first data taken
 		{
-			fstream elementDescription;
-			elementDescription.open((resultFolder+DIST_ELEMENT_DESCRIPTION).c_str(), ios::out | ios::app);
-			elementDescription<<"# sample element:\n\t";
 			for (int i=0;i<nObserver;i++)
 			{
 				highBound[i]=lowBound[i]=element_alias[i];	// initiate highBound and lowBound value;
-				elementDescription<<element_alias[i]<<'\t';
-				observerZero[i]=element_alias[i]/observerRange[i];
 			}
-			elementDescription<<endl;
-			elementDescription.close();
-				
 			sampleTaken=1;
 		}
 		else
@@ -175,7 +195,32 @@ public:
 // save the current distribution into file
 	void saveDistribution()
 	{
+		struct stat sb; 	// check existance of folder
 		FILE * distFile;
+		fstream filePointer;
+
+	// generate folder if not exist
+		if (stat(resultFolder.c_str(), &sb) != 0) 	mkdir(resultFolder.c_str(),0755);
+	// save description
+		// write the storage scheme to file
+		filePointer.open((resultFolder+DIST_ELEMENT_DESCRIPTION).c_str(), ios::out | ios::trunc);
+		filePointer<<"# This file contains the description for the distribution.\n";
+		filePointer<<"# Observer Number: \n\t"<<nObserver<<endl;
+		filePointer<<"# Range:\n\t";
+		for (int i=0;i<nObserver;i++)
+			filePointer<<observerRange[i]<<'\t';
+		filePointer<<"\n# Compress level: \n\t";
+		for (int i=0;i<nObserver;i++)
+			filePointer<< compressLevel[i]<<'\t';
+		filePointer<<endl;
+		filePointer<<"# boundary description:\n";
+		for (int i=0;i<nObserver;i++)
+		{
+			filePointer<<"#\tobserver_"<<i<<endl<<"\t\t"
+			<<lowBound[i]<<"\t"<<highBound[i]<<endl;
+		}
+		filePointer.close();
+	// save distribution;
 		distFile=fopen((resultFolder+DIST_COUNTING).c_str(), "w");
 		for (long i=0;i<allocatedSize;i++)
 		{
@@ -186,8 +231,29 @@ public:
 	}
 
 // export the distribution in a format of: 	ob1 ob2 ... obn [count]
-	void exportDistribution()
-	
+// currently only support exporting from in memory distribution
+// Result is the same as saveDistribution, but the ID is replaced by
+// the amount of each observers.
+	void exportDistribution(string & export_file_name)
+	{
+		int index_temp[MAX_VARIABLE];
+		FILE exportFilePointer();
+
+		exportFilePointer=fopen(export_file_name.c_str(), "w");
+		for (long i=0;i<allocatedSize;i++)
+		{
+			if(observer[i]!=0)
+			{
+				ID2Index(index_temp,i);
+				for (int j=0;j<nObserver;j++)
+				{
+					fprintf(exportFilePointer, "%8d\t",index_temp[j]);	
+				}
+				fprintf(exportFilePointer, "%20ld\n",observer[i]);
+			}
+		}
+		fclose(exportFilePointer);
+	}
 	
 // initialize storage scheme(used when creating new distribution)
 	void initiateScheme()
@@ -256,34 +322,63 @@ public:
 // load distribution description from file
 	void loadScheme()
 	{
+		stringstream ss(func_buffer);
 		fstream elementDescription;
 		string func_buffer;
+		int tempSize=1;
+		int tempCompress=1;
+
 		elementDescription.open((resultFolder+DIST_ELEMENT_DESCRIPTION).c_str(), ios::in);
-		while(getline(elementDescription, func_buffer) && func_buffer[0]!='#')
+		do
 		{
-			stringstream ss(func_buffer);
-			ss>>Observer;
+			if(!(getline(elementDescription, func_buffer))) 	exit(1);
 		}
-		while(getline(elementDescription, func_buffer) && func_buffer[0]!='#')
+		while(func_buffer[0]=='#')
+		ss<<func_buffer;
+		ss>>nObserver;
+		do
 		{
-			stringstream ss(func_buffer);
-			for (i=0;i<nObserver;i++) 	ss>>observerRange[i];
+			if(!(getline(elementDescription, func_buffer))) 	exit(1);
 		}
-		while(getline(elementDescription, func_buffer) && func_buffer[0]!='#')
+		while(func_buffer[0]=='#')
+		ss<<func_buffer;
+		for (int i=0;i<nObserver;i++)
 		{
-			stringstream ss(func_buffer);
-			for (i=0;i<nObserver;i++) 	ss>>compressLevel[i];
+			ss>>observerRange[i];
+			tempSize*=observerRange[i];
 		}
-		while(getline(elementDescription, func_buffer) && func_buffer[0]!='#')
+		do
 		{
-			stringstream ss(func_buffer);
-			for (i=0;i<nObserver;i++) 
+			if(!(getline(elementDescription, func_buffer))) 	exit(1);
+		}
+		while(func_buffer[0]=='#')
+		ss<<func_buffer;
+		for (int i=0;i<nObserver;i++)
+		{
+			ss>>compressLevel[i];
+			tempCompress*=compressLevel[i];
+		}
+		for (int i=0;i<nObserver;i++)
+		{
+			do
 			{
-				ss>>observerZero[i];
-				observerZero[i]=(observerZero[i]/observerRange[i])*observerRange[i];
+				if(!(getline(elementDescription, func_buffer))) 	exit(1);
+			}
+			while(func_buffer[0]=='#')
+			ss<<func_buffer;
+			{
+				ss>>lowBound[i];
+				ss>>highBound[i];
 			}
 		}
 		elementDescription.close();
+	// determine baseline with lowBound[i];
+		for (int i=0; i<nObserver;i++)
+		{
+			observerZero[i]=(lowBound[i]/observerRange[i])*observerRange[i];
+		}
+	// determine the memory size need to be allocated.
+		allocatedSize=tempSize/tempCompress;
 	}
 };
 
